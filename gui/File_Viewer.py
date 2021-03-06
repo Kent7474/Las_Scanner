7b577bda472e89d34792b48a371783dc6ed7e225
Kent7474 <fh.ambrosio@gmail.com>
2021-03-05 22:59:53 -0300

    V1.1

--- gui/File_Viewer.py
+++ gui/File_Viewer.py
@@ -4,7 +4,6 @@ import os.path
 
 class LasViewer:
     moutList = []
-    listSelectLas = ""
 
     def __init__(self):
 
@@ -48,20 +47,22 @@ class LasViewer:
 
         self.window = sg.Window("LAS Viewer", self.layout)
 
+        # ----------------------------------------------------------------
         # MAIN WHILE WINDOW
+        # -----------------------------------------------------------------
         while True:
             self.event, self.values = self.window.read()
             print(self.event)
+
             if self.event == "Exit" or self.event == sg.WIN_CLOSED:
                 break
 
             self.__folder(self.event, self.values, self.window)
-
             if self.event == "Clear":
-                self.__clear(self.window, self.listSelectLas)
-                self.listSelectLas = ""
-
-            self.__select_item_from_list(self.event, self.values, self.window, self.moutList, self.listSelectLas)
+                self.__clear(self.window, self.event)
+                self.moutList = []
+            self.__select_item_from_list(self.event, self.values, self.window, self.moutList)
+            self.__list_button(self.event, self.moutList)
 
     # SELECT FOLDER AND LIST THE FILE NAMES FUNCTION
     @staticmethod
@@ -85,19 +86,19 @@ class LasViewer:
 
     # CLEAR FILE LIST FUNCTION
     @staticmethod
-    def __clear(window, listSelectLas):
+    def __clear(window, event):
         """CLEAR FILE LIST FUNCTION"""
         window["-MOUT-"].update("")
-        print(listSelectLas)
 
     @staticmethod
-    def __select_item_from_list(event, values, window, moutList, listSelectLas):
-        print(listSelectLas)
+    def __select_item_from_list(event, values, window, moutList):
+        """SELECT A .LAS FILE AND ADD TO THE LIST THAT WILL USE TO READ"""
         if event == "-FILE LIST-":
             try:
                 fileListed = values["-FILE LIST-"][0]
-                moutList.append(fileListed)
-                print(fileListed)
+                if fileListed not in moutList:
+                    moutList.append(fileListed)
+                    print(fileListed)
                 listSelectLas = ""
                 for f in moutList:
                     listSelectLas += f + "\n"
@@ -105,5 +106,35 @@ class LasViewer:
             except:
                 print("Please select File Folder containing *.las")
 
+    @staticmethod
+    def __list_button(event, moutList):
+        if event == "List":
+            print(moutList)
 
-las = LasViewer()
+
+class LasScanned:
+
+    def __init__(self):
+        self.file_scanned = [
+            [
+                sg.Text("File Folder"),
+                sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+                sg.FolderBrowse(),
+            ],
+            [
+                sg.Listbox(values=[], enable_events=True, size=(50, 30), key="-FILE LIST-"),
+            ],
+            [
+                sg.HSeparator()
+            ],
+            [
+                sg.Button("Exit", size=(5, 1)),
+
+            ]
+        ]
+
+        self.window_scanned = sg.Window("Las Scanned", self.file_scanned).read()
+
+
+lasViewerSurface = LasViewer()
+lasScannedSurface = LasScanned()
